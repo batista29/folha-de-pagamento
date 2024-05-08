6ad472cd2d4e43a62a727e438e9c52007b7214ec
Natã Batista <98469905+batista29@users.noreply.github.com>
2024-05-08 10:37:25 -0300

    Iniciando

---
+++ workspace/main.py
@@ -0,0 +1,22 @@
+# Folha de pagamento em python
+
+def adicionarFuncionario():
+    print("")
+
+def removerFuncionario():
+    print("")
+    
+def relatorioFuncionario(): #de um unico funcionario
+    print("")
+    
+def relatorioDosFuncionarios(): #de todos os funcionarios
+    print("")
+    
+def maiorSalario(): #Infos funcionario com maior salario
+    print("")
+    
+def maisFaltas(): #Infos funcionario com mais faltas
+    print("")
+
+print("O que deseja fazer?\n")
+print("")
