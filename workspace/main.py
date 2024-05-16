99739b9cc0a47154cf4cce5b39a408a007f9e987
esterAmaral <163193914+esterAmaral@users.noreply.github.com>
2024-05-16 08:19:48 -0300

    adicionar funcionário e relatório individual funcionando

--- workspace/main.py
+++ workspace/main.py
@@ -1,30 +1,39 @@
 # Folha de pagamento em python
 funcionarios = {}
-def adicionarFuncionario(): # Ester
-    count = 0
-    n = int(input("Quantos funcionários deseja adicionar? "))
-    while count < n:
-        pessoa = []
-        matricula = input("Digite a matrícula do funcionário: ")
-        nome =  input("Nome do funcioário: ")
-        cod_funcao = int(input("Digite o código da função do funcionário: "))
-        num_faltas = int(input("Quantidade de faltas no mês: "))
-        salarioBruto = float(input("Salário Bruto do funcionário: "))
-        pessoa.append(nome)
-        pessoa.append(cod_funcao)
-        pessoa.append(num_faltas)
-        pessoa.append(salarioBruto)
-        print(pessoa)
-        funcionarios[matricula] = pessoa
-        count+=1
-    return funcionarios
+def lerDados():
+    matricula = input("Digite a matrícula do funcionário: ")
+    nome =  input("Nome do funcioário: ")
+    cod_funcao = int(input("Digite o código da função do funcionário: "))
+    num_faltas = int(input("Quantidade de faltas no mês: "))
+    salarioBruto = float(input("Salário Bruto do funcionário: "))
+    return matricula,nome,cod_funcao,num_faltas,salarioBruto
+def adicionarFuncionario(matricula,nome,cod_funcao,num_faltas,salarioBruto): # Ester
+    pessoa = []
+    pessoa.append(nome)
+    pessoa.append(cod_funcao)
+    pessoa.append(num_faltas)
+    pessoa.append(salarioBruto)
+    print(pessoa)
+    funcionarios[matricula] = pessoa
+    #return funcionarios
 
 def removerFuncionario(): # Geovana
     print("")
     
-def relatorioFuncionario(relatorio_func): #de um unico funcionario - Ester
+def relatorioFuncionario(funcionarios,matricula): #de um unico funcionario - Ester
    #Matrícula, Nome, Código da Função, Salário Bruto e Salário Líquido de cada funcionário
-    print()
+    salario_liquido = 0
+    print("\nRelatório do Funcionário\n")
+    print("Nº matrícula: "+matricula)
+    print("Nome: "+ funcionarios[matricula][0])
+    if funcionarios[matricula][1] == 101:
+        funcao = "Vendedor"
+    elif funcionarios[matricula][1] == 102:
+        funcao = "Administrativo"
+    print(f"Código da Função: {funcionarios[matricula][1]} - {funcao}")
+    print(f"Quantidade de faltas no mês: {funcionarios[matricula][2]}")
+    print(f"Salário Bruto: {funcionarios[matricula][3]}")
+    print(f"Salário Líquido:  {salario_liquido}")
 
 def relatorioDosFuncionarios(): #de todos os funcionarios - Natã
     print("")
@@ -37,21 +46,29 @@ def maisFaltas(): #Infos funcionario com mais faltas - Natã
 
 print("O que deseja fazer?\n","")
 opcao = int(input("1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n"))
+while opcao !=0:
+    
+    if(opcao == 1):
+        matricula,nome,cod_funcao,num_faltas,salarioBruto = lerDados()
+        adicionarFuncionario(matricula,nome,cod_funcao,num_faltas,salarioBruto)
+    elif(opcao == 2):
+        removerFuncionario(funcionarios,matricula)
+    elif(opcao == 3):
+        matricula = input("Por Favor digite nº da matricula do funcionário que deseja ver relatório: ")
+        relatorioFuncionario(funcionarios,matricula)
 
-if(opcao == 1):
-   adicionarFuncionario()
-elif(opcao == 2):
-   removerFuncionario()
-elif(opcao == 3):
-   relatorioFuncionario()
-elif(opcao == 4):
-   relatorioDosFuncionarios()
-elif(opcao == 5):
-   maiorSalario()
-elif(opcao == 6):
-   maisFaltas()
-else:
-   print("Essa opcao não existe")
-
+    elif(opcao == 4):
+        funcionarios = adicionarFuncionario()
+        relatorioDosFuncionarios()
+    elif(opcao == 5):
+        maiorSalario()
+    elif(opcao == 6):
+        maisFaltas()
+    else:
+        print("Essa opcao não existe!")
+    print("O que deseja fazer?\n","")
+    opcao = int(input("1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n"))
 # Duvidas
 # A matricula deve ser int ou string?
+#posso armazenar mais dados ou precisa ser somente os dados listados?
+# como funciona o salário bruto?
