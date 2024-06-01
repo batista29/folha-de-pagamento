8b2995dea003b5a71255966c6e66bf1e0831fcc5
batista29 <natabatista2908@gmail.com>
2024-06-01 19:53:56 -0300

    erros remanescentes arrumados

--- workspace/revisar.py
+++ workspace/revisar.py
@@ -1,37 +1,38 @@
 # Folha de pagamento em python
 funcionarios = {}
 salarioLiq = 0
-def descontoPorFaltas(num_faltas,salarioBruto): #Ester
+
+def descontoPorFaltas(num_faltas,salarioBruto,salarioFixo):
     if(num_faltas != 0):
-        salarioBruto = salarioBruto - (num_faltas*(salarioBruto/30))
+        salarioBruto = salarioBruto - (num_faltas*(salarioFixo/30))
     else:
         salarioBruto = salarioBruto
     return salarioBruto 
 
-def lerDados(funcionarios):#Ester
+def lerDados(funcionarios):
     matricula = int(input("Digite a matrícula do funcionário: "))
 
     while(matricula in funcionarios.keys()):
-        matricula = int(input("Esse nº de matrícula já existe, digite novamente:  "))
+        matricula = int(input("Esse nº de matrícula já existe, digite outra:  "))
     else:
         nome =  input("Nome do funcioário: ")
-        cod_funcao = int(input("Digite o código da função do funcionário: "))
-        num_faltas = int(input("Quantidade de faltas no mês: "))
+        cod_funcao = int(input("Digite o código da função do funcionário (101 ou 102): "))
         if cod_funcao == 101:
             sal_fixo = 1500
-            volume_venda = int(input("Digite o valor de vendas no mês:"))
+            volume_venda = float(input("Digite o valor de vendas no mês:"))
             comissao = volume_venda*0.09 
-            salBruto = (sal_fixo+comissao)
+            salarioBruto = (sal_fixo+comissao)
         elif cod_funcao == 102:
             sal_fixo = float(input("Salário do funcionário (R$ 2150 - 6950): "))
             while sal_fixo < 2150 or sal_fixo > 6950:
                 print("Faixa salarial é de R$2150 a R$6950!")
                 sal_fixo = float(input("Salário do funcionário (R$ 2150 - 6950): "))
-            salBruto = sal_fixo
-        salarioBruto = descontoPorFaltas(num_faltas,salBruto)
+            salarioBruto = sal_fixo
+        num_faltas = int(input("Quantidade de faltas no mês: "))
+        salarioBruto = descontoPorFaltas(num_faltas,salarioBruto,sal_fixo)
     return matricula,nome,cod_funcao,num_faltas,salarioBruto
 
-def adicionarFuncionario(matricula,nome,cod_funcao,num_faltas,salarioBruto): # Ester
+def adicionarFuncionario(matricula,nome,cod_funcao,num_faltas,salarioBruto):
     pessoa = []
     pessoa.append(nome)
     pessoa.append(cod_funcao)
@@ -39,9 +40,10 @@ def adicionarFuncionario(matricula,nome,cod_funcao,num_faltas,salarioBruto): # E
     pessoa.append(salarioBruto)
     
     funcionarios[matricula] = pessoa
-    print(funcionarios)
-    #return funcionarios
-def salarioLiquido(salarioBruto):#Ester
+    print("Funcionário adicionado com sucesso")
+    return funcionarios
+
+def salarioLiquido(salarioBruto):
     if salarioBruto <=2259.20:
         salarioLiq = salarioBruto
         percentual = 0
@@ -63,19 +65,17 @@ def salarioLiquido(salarioBruto):#Ester
         salarioLiq = salarioBruto - imposto
     return salarioLiq,percentual
 
-def removerFuncionario(funcionarios): #Geovana
-    excluir = input("Digite a matricula do funcionario que deseja remover: ")
-    if excluir in funcionarios:
+def removerFuncionario(funcionarios):
+    excluir = int(input("Digite a matricula do funcionario que deseja remover: "))
+    while not (excluir in funcionarios.keys()):
+        excluir = int(input(f"Funcionário com matrícula {excluir} não encontrado.\nDigite um funcionario valido"))
+    else:
         del funcionarios[excluir]
         print(f"Funcionário com matrícula {excluir} removido com sucesso.")
-    else:
-        print(f"Funcionário com matrícula {excluir} não encontrado.")
 
-def relatorioFuncionario(funcionarios,matricula,salario_liquido,percentual): #de um unico funcionario - Ester
-   #Matrícula, Nome, Código da Função, Salário Bruto e Salário Líquido de cada funcionário
-
-    print(funcionarios)
-    print("\nRelatório do Funcionário\n")
+def relatorioFuncionario(funcionarios,matricula,salario_liquido,percentual):
+    
+    print("\nFolha de pagamento\n")
     print("Nº matrícula: ",matricula)
     print("Nome: "+ funcionarios[matricula][0])
     if funcionarios[matricula][1] == 101:
@@ -88,26 +88,14 @@ def relatorioFuncionario(funcionarios,matricula,salario_liquido,percentual): #de
     print(f"Salário Líquido:  {salario_liquido}")
     print(f"Alíquota:  {percentual}")
 
-def relatorioDosFuncionarios(funcionarios): #de todos os funcionarios - Natã
+def relatorioDosFuncionarios(funcionarios):
 
     for matricula in (funcionarios.keys()):
-         #verificar se a formula esta correta
-        salario_descontado = funcionarios[matricula][3]-(((funcionarios[matricula][3])/30)*funcionarios[matricula][2])
-
-        if (salario_descontado <= 2259.2):
-            salario_liquido = salario_descontado
-        elif(2259.21 <= salario_descontado <= 2828.65):
-            salario_liquido = salario_descontado-(salario_descontado*0.075)
-        elif(2828.66 <= salario_descontado <= 3751.05):
-            salario_liquido = salario_descontado-(salario_descontado*0.15)
-        elif(3751.06 <= salario_descontado <= 4664.68):
-            salario_liquido = salario_descontado-(salario_descontado*0.225)
-        else:
-            salario_liquido = salario_descontado-(salario_descontado*0.275)
-        porcentagem_desconto = 100-((salario_liquido/funcionarios[matricula][3])*100)
+        salarioBruto = funcionarios[matricula][3]
+        salario_liquido,porcentagem_desconto = salarioLiquido(salarioBruto)
         print(f"Matricula: {matricula}, Nome: {funcionarios[matricula][0]}, Código da função: {funcionarios[matricula][1]},Faltas: {funcionarios[matricula][2]}, Salário bruto: {funcionarios[matricula][3]}, Salário liquido: {salario_liquido}, Porcentagem de desconto: {porcentagem_desconto}%\n")
 
-def maiorSalario(funcionarios): #Infos funcionario com maior salario - Geovana
+def maiorSalario(funcionarios):
     maior_salario = 0
 
     for matricula in (funcionarios.keys()):
@@ -120,19 +108,20 @@ def maiorSalario(funcionarios): #Infos funcionario com maior salario - Geovana
 
     print(f"Matricula: {matriculafuncionario}, {funcionarios[matriculafuncionario][0]}, Código da função: {funcionarios[matriculafuncionario][1]}, Faltas: {funcionarios[matriculafuncionario][2]}, Salário bruto: {funcionarios[matriculafuncionario][3]}, Salário Líquido: {salario_liquido} ")
             
+def maisFaltas(funcionarios):
+    maior_num_faltas = 0
     
-def maisFaltas(funcionarios): #Infos funcionario com mais faltas - Natã
-    mais_faltas = 0
-
     for matricula in (funcionarios.keys()):
-        if(mais_faltas < funcionarios[matricula][2]):
-            mais_faltas = funcionarios[matricula][2]
-            matricula_func = matricula
-
-    print("Matricula: ", matricula_func,"\nNome: ", funcionarios[matricula_func][0], ", Código da função: ", funcionarios[matricula_func][1], ", Faltas: ", funcionarios[matricula_func][2], ", Salário bruto: ", funcionarios[matricula_func][3],"\n")
-
-print("O que deseja fazer?\n","")
-opcao = int(input("1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n"))
+        if(maior_num_faltas < funcionarios[matricula][2]):
+            maior_num_faltas = funcionarios[matricula][2]
+    print("Funcionário(s) com mais faltas:")
+    for matricula in (funcionarios.keys()):
+        if(0 != maior_num_faltas == funcionarios[matricula][2]):
+            print("Matricula: ", matricula,", Nome: ", funcionarios[matricula][0], ", Código da função: ", funcionarios[matricula][1], ", Faltas: ", funcionarios[matricula][2], ", Salário bruto: ", funcionarios[matricula][3])
+        else:
+            print("Não temos faltas no sistema")
+print("MENU\n")
+opcao = int(input("1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n\nEscolha uma das opções acima: \n"))
 
 while opcao !=0:
     if(opcao == 1):
@@ -142,9 +131,12 @@ while opcao !=0:
         removerFuncionario(funcionarios)
     elif(opcao == 3):
         matricula = int(input("Por Favor digite nº da matricula do funcionário que deseja ver relatório: "))
-        salarioBruto = funcionarios[matricula][3]
-        salarioLiq,percentual = salarioLiquido(salarioBruto)
-        relatorioFuncionario(funcionarios,matricula,salarioLiq,percentual)
+        while not(matricula in funcionarios.keys()):
+            matricula = int(input("Por Favor digite nº de matricula existente: "))
+        else:
+            salarioBruto = funcionarios[matricula][3]
+            salarioLiq,percentual = salarioLiquido(salarioBruto)
+            relatorioFuncionario(funcionarios,matricula,salarioLiq,percentual)
     elif(opcao == 4):
         relatorioDosFuncionarios(funcionarios)
     elif(opcao == 5):
@@ -156,10 +148,5 @@ while opcao !=0:
     else:
         print("Essa opcao não existe!")
         
-    opcao = int(input("O que deseja fazer?\n\n1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n"))
-
-# Duvidas
-# A matricula deve ser int ou string?
-# como calcular a comissão de venda dos vendedores?   
-#posso armazenar mais dados ou precisa ser somente os dados listados?
-# como funciona o salário bruto?
+    print("MENU\n")
+    opcao = int(input("1-Adicionar funcionario\n2-Remover funcionario\n3-Ver relatorio do funcionario\n4-Ver relatorios dos funcionarios\n5-Ver o maior salario\n6-Ver funcionario com mais faltas\n\nEscolha uma das opções acima: \n"))
